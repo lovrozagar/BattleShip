f796321ff33beb5c6b661f07b0752b857c5de489
lovro <lovro.zagar5@gmail.com>
2023-03-07 13:13:05 +0000

    feat: add coordinates to loadBoard

--- src/modules/DOM/helper.js
+++ src/modules/DOM/helper.js
@@ -11,16 +11,56 @@ const helper = (() => {
     board.id = `board-${friendlyOrEnemy}`
     board.classList.add('board', friendlyOrEnemy)
 
+    loadLetters(board)
+    loadNumbers(board)
+    loadFields(board)
+
+    container.appendChild(board)
+  }
+
+  function loadLetters(container) {
+    const letterContainer = document.createElement('div')
+    letterContainer.classList = 'letter-container'
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+    letters.forEach((element) => {
+      const letter = document.createElement('div')
+      letter.className = 'letter'
+      letter.textContent = element
+      letterContainer.appendChild(letter)
+    })
+
+    container.appendChild(letterContainer)
+  }
+
+  function loadNumbers(container) {
+    const numberContainer = document.createElement('number-container')
+    numberContainer.classList = 'number-container'
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    numbers.forEach((element) => {
+      const number = document.createElement('div')
+      number.classList = 'number'
+      number.textContent = element
+      numberContainer.appendChild(number)
+    })
+
+    container.appendChild(numberContainer)
+  }
+
+  function loadFields(container) {
+    const fieldContainer = document.createElement('field-container')
+    fieldContainer.className = 'field-container'
+
     for (let i = 0; i < BOARD_SIZE; i += 1) {
       for (let j = 0; j < BOARD_SIZE; j += 1) {
         const field = document.createElement('div')
         field.className = 'field'
-
-        board.appendChild(field)
+        fieldContainer.appendChild(field)
       }
     }
 
-    container.appendChild(board)
+    container.appendChild(fieldContainer)
   }
 
   return { deleteAppContent, loadBoard }
