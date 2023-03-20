29722054121bf60fdb44f4706989efa1a3d97198
lovro <lovro.zagar5@gmail.com>
2023-03-20 20:58:05 +0000

    feat: player/cpu retains turn on hit

--- src/modules/DOM/battle.js
+++ src/modules/DOM/battle.js
@@ -6,6 +6,7 @@ import Message from '../utils/message'
 import Sound from '../utils/sound'
 
 const Battle = (() => {
+  // MAIN CALLER
   function loadBattleContent() {
     helper.deleteAppContent()
 
@@ -23,6 +24,8 @@ const Battle = (() => {
 
     styleOnTurn(document.querySelector('.message.battle.agent'))
   }
+
+  // COMPONENTS
 
   function createBattleWrapper() {
     const wrapper = helper.create('div', { className: 'battle-wrapper' })
@@ -98,152 +101,8 @@ const Battle = (() => {
     return overlay
   }
 
-  function initNewGameButton() {
-    const button = document.getElementById('new-game-button')
-    button.addEventListener('click', () => window.location.reload())
-  }
-
-  function displayPlayerShips() {
-    const friendlyBoard = document.getElementById('field-container-friendly')
-    Game.getState().getPlayer().getMap().setAllShipsNotFound()
-    fleet.loadFleet(friendlyBoard)
-  }
-
-  async function handleFieldClick(event) {
-    const { target } = event
-    disableField(target)
-
-    // END GAME IF PLAYER WIN BEFORE CPU FIRES AUTOMATICALLY
-    const playerIsWinner = await playerPlays(target)
-    if (playerIsWinner) return
-
-    await cpuPlays()
-  }
-
-  async function playerPlays(fieldNode) {
-    unInitBoardFields()
-
-    const enemyMessage = document.querySelector('.message.battle.enemy')
-    const agentMessage = document.querySelector('.message.battle.agent')
-
-    Sound.shot()
-    await timeoutHalfSecond()
-
-    const cpu = Game.getState().getCPU()
-    const index = [...fieldNode.parentNode.children].indexOf(fieldNode)
-    const [row, col] = helper.getCoordinatesFromIndex(index)
-
-    const boardElement = cpu.getMap().getBoard()[row][col]
-    const shipName = getShipNameFromBoard(boardElement)
-    const battleship = cpu.getMap().getShip(shipName)
-
-    console.log(cpu.getMap().getBoard())
-
-    switch (boardElement) {
-      case 'x':
-        addMissStyle(fieldNode)
-        await timeoutMissileLength()
-        Sound.miss()
-        break
-      default:
-        addHitStyle(fieldNode)
-        playerHits({ cpu, battleship, row, col })
-        await timeoutMissileLength()
-        Sound.hit()
-    }
-
-    console.log('is player winner: ', cpu.isLoser())
-    console.log(cpu.getMap())
-
-    displayPlayerMessage(boardElement, battleship)
-
-    await timeoutOneSecond()
-    if (cpu.isLoser()) {
-      showPlayerWinModal()
-      return true
-    }
-    await timeoutOneAndHalfSecond()
-
-    styleOffTurn(agentMessage)
-    styleOnTurn(enemyMessage)
-    resizePlayerOffTurn()
-
-    return false
-  }
-
-  async function cpuPlays() {
-    const enemyMessage = document.querySelector('.message.battle.enemy')
-    const agentMessage = document.querySelector('.message.battle.agent')
-
-    displayPlayerNoCommentMessage()
-
-    await timeoutOneSecond()
-    Sound.shot()
-    await timeoutHalfSecond()
-
-    const friendlyBoard = document.getElementById('field-container-friendly')
-    const player = Game.getState().getPlayer()
-    const [row, col] = player.cpuPlay()
-
-    const boardElement = player.getMap().getBoard()[row][col]
-    const shipName = getShipNameFromBoard(boardElement)
-    const battleship = player.getMap().getShip(shipName)
-    const index = helper.getIndexFromCoordinates(row, col)
-
-    switch (boardElement) {
-      case 'miss':
-        addMissStyle(friendlyBoard.children[index])
-        player.getMap().getBoard()[row][col] = 'miss'
-        await timeoutMissileLength()
-        Sound.miss()
-        break
-      default:
-        addHitStyle(friendlyBoard.children[index])
-        player.getMap().getBoard()[row][col] = 'hit'
-        await timeoutMissileLength()
-        Sound.hit()
-    }
-
-    console.log('is cpu winner: ', player.isLoser())
-
-    displayEnemyMessage(boardElement, battleship)
-
-    await timeoutOneSecond()
-    if (player.isLoser()) {
-      showEnemyWinModal()
-      return
-    }
-    await timeoutOneAndHalfSecond()
-
-    styleOffTurn(enemyMessage)
-    styleOnTurn(agentMessage)
-    resizePlayerOnTurn()
-    initBoardFields()
-  }
-
-  function displayBattleStartMessage(character) {
-    const message = document.getElementById(`message-${character}`)
-    if (character === 'agent')
-      Component.addTypeWriterMessage(message, Message.getBattleStartMessage())
-    else
-      Component.addTypeWriterMessage(
-        message,
-        Message.getNewEnemyBattleStartMessage()
-      )
-  }
-
-  function displayWinMessage(character) {
-    const message = document.getElementById(`message-${character}`)
-    if (character === 'agent-win')
-      Component.addTypeWriterMessage(message, Message.getPlayerWinMessage())
-    if (character === 'enemy-win')
-      Component.addTypeWriterMessage(message, Message.getEnemyWinMessage())
-  }
-
   function showPlayerWinModal() {
     const app = document.getElementById('app')
-
-    app.classList.add('of-focus')
 
     helper.appendAll(app, [
       createWinnerModal({
@@ -257,12 +116,12 @@ const Battle = (() => {
     displayWinMessage('agent-win')
     initNewGameButton()
     unInitBoardFields()
+
+    return 'win'
   }
 
   function showEnemyWinModal() {
     const app = document.getElementById('app')
-
-    app.classList.add('of-focus')
 
     helper.appendAll(app, [
       createWinnerModal({
@@ -275,9 +134,229 @@ const Battle = (() => {
 
     displayWinMessage('enemy-win')
     initNewGameButton()
+
+    return 'win'
   }
 
-  // TODO: dont allow same class
+  // LISTENERS
+
+  function initNewGameButton() {
+    const button = document.getElementById('new-game-button')
+    button.addEventListener('click', () => window.location.reload())
+  }
+
+  function initBoardFields() {
+    const enemyMap = document.getElementById('board-enemy')
+    const enemyBoard = enemyMap.querySelector('.field-container')
+    enemyBoard.childNodes.forEach((field) => {
+      field.addEventListener('click', handleFieldClick)
+    })
+    addFieldHoverWhenOnTurn()
+  }
+
+  function unInitBoardFields() {
+    const fields = document.querySelectorAll('.field')
+    fields.forEach((field) =>
+      field.removeEventListener('click', handleFieldClick)
+    )
+    removeFieldHoverWhenOffTurn()
+  }
+
+  // HANDLERS
+
+  async function handleFieldClick(event) {
+    const { target } = event
+    disableField(target)
+    // Player
+    const playerTurn = await playerPlays(target)
+    if (playerTurn === 'win' || playerTurn === 'hit') return
+    // Cpu
+    let cpuTurn = await cpuPlays()
+    if (cpuTurn === 'win') return
+    while (cpuTurn === 'hit') cpuTurn = await cpuPlays()
+  }
+
+  // PLAYER/CPU TURNS
+
+  async function playerPlays(fieldNode) {
+    const cpu = Game.getState().getCPU()
+    const index = [...fieldNode.parentNode.children].indexOf(fieldNode)
+    const [row, col] = helper.getCoordinatesFromIndex(index)
+
+    const boardElement = cpu.getMap().getBoard()[row][col]
+    const shipName = getShipNameFromBoard(boardElement)
+    const battleship = cpu.getMap().getShip(shipName)
+
+    console.log(cpu.getMap().getBoard())
+    unInitBoardFields()
+    await shotOnTurnPlay('player')
+
+    if (boardElement === 'x') await playerMiss(fieldNode)
+    else return await playerHit(fieldNode)
+
+    displayPlayerMessage(boardElement, battleship)
+    await timeoutOneSecond()
+    await turnEnd('player')
+
+    return 'miss'
+  }
+
+  async function cpuPlays() {
+    const player = Game.getState().getPlayer()
+    const [row, col] = player.cpuPlay()
+
+    const boardElement = player.getMap().getBoard()[row][col]
+    const shipName = getShipNameFromBoard(boardElement)
+    const battleship = player.getMap().getShip(shipName)
+
+    console.log(row, col)
+
+    await shotOnTurnPlay('cpu')
+
+    if (boardElement === 'miss') await cpuMiss(row, col)
+    else return await cpuHit(row, col)
+
+    displayEnemyMessage(boardElement, battleship)
+    await timeoutOneSecond()
+    await turnEnd('cpu')
+
+    return 'miss'
+  }
+
+  // TURN HELPERS
+
+  async function shotOnTurnPlay(playerOrCpu) {
+    if (playerOrCpu === 'player') {
+      Sound.shot()
+      await timeoutHalfSecond()
+    } else {
+      displayPlayerNoCommentMessage()
+      await timeoutOneSecond()
+      Sound.shot()
+      await timeoutHalfSecond()
+    }
+  }
+
+  async function playerMiss(fieldNode) {
+    addMissStyle(fieldNode)
+    await timeoutMissileLength()
+    Sound.miss()
+  }
+
+  async function cpuMiss(row, col) {
+    const friendlyBoard = document.getElementById('field-container-friendly')
+    const player = Game.getState().getPlayer()
+    const index = helper.getIndexFromCoordinates(row, col)
+
+    addMissStyle(friendlyBoard.children[index])
+    player.getMap().getBoard()[row][col] = 'miss'
+    await timeoutMissileLength()
+    Sound.miss()
+  }
+
+  async function playerHit(fieldNode) {
+    const cpu = Game.getState().getCPU()
+    const index = [...fieldNode.parentNode.children].indexOf(fieldNode)
+    const [row, col] = helper.getCoordinatesFromIndex(index)
+
+    const boardElement = cpu.getMap().getBoard()[row][col]
+    const shipName = getShipNameFromBoard(boardElement)
+    const battleship = cpu.getMap().getShip(shipName)
+
+    addHitStyle(fieldNode)
+    loadShipIfSunk({ cpu, battleship, row, col })
+    await timeoutMissileLength()
+    Sound.hit()
+    displayPlayerMessage(boardElement, battleship)
+
+    await timeoutOneSecond()
+    if (cpu.isLoser()) return showPlayerWinModal()
+    initBoardFields()
+
+    return 'hit'
+  }
+
+  async function cpuHit(row, col) {
+    const friendlyBoard = document.getElementById('field-container-friendly')
+    const player = Game.getState().getPlayer()
+    const index = helper.getIndexFromCoordinates(row, col)
+
+    const boardElement = player.getMap().getBoard()[row][col]
+    const shipName = getShipNameFromBoard(boardElement)
+    const battleship = player.getMap().getShip(shipName)
+
+    addHitStyle(friendlyBoard.children[index])
+    player.getMap().getBoard()[row][col] = 'hit'
+    await timeoutMissileLength()
+    Sound.hit()
+    displayEnemyMessage(boardElement, battleship)
+    if (player.isLoser()) return showEnemyWinModal()
+
+    return 'hit'
+  }
+
+  async function turnEnd(playerOrCpu) {
+    await timeoutOneAndHalfSecond()
+
+    if (playerOrCpu === 'player') {
+      styleOffTurn(document.querySelector('.message.battle.agent'))
+      styleOnTurn(document.querySelector('.message.battle.enemy'))
+      resizePlayerOffTurn()
+    } else {
+      styleOffTurn(document.querySelector('.message.battle.enemy'))
+      styleOnTurn(document.querySelector('.message.battle.agent'))
+      resizePlayerOnTurn()
+      initBoardFields()
+    }
+  }
+
+  // FLEET
+
+  function displayPlayerShips() {
+    const friendlyBoard = document.getElementById('field-container-friendly')
+    Game.getState().getPlayer().getMap().setAllShipsNotFound()
+    fleet.loadFleet(friendlyBoard)
+  }
+
+  function loadShipIfSunk(data) {
+    const board = document.getElementById('field-container-enemy')
+    const map = data.cpu.getMap()
+    const boardArray = map.getBoard()
+
+    map.receiveAttack([data.row, data.col])
+    if (data.battleship.isSunk) {
+      const element = boardArray[data.row][data.col]
+      const [row, col] = findOrigin(boardArray, boardArray[data.row][data.col])
+      fleet.loadShipOnBoard(data.cpu, { map, board, element, row, col })
+    }
+  }
+
+  function findOrigin(board, element) {
+    for (let i = 0; i < board.length; i += 1) {
+      for (let j = 0; j < board[0].length; j += 1) {
+        if (board[i][j] === element) return [i, j]
+      }
+    }
+    return [0, 0]
+  }
+
+  function getShipNameFromBoard(boardElement) {
+    return boardElement.slice(0, boardElement.length - 1)
+  }
+
+  // MESSAGES
+
+  function displayBattleStartMessage(character) {
+    const message = document.getElementById(`message-${character}`)
+    if (character === 'agent')
+      Component.addTypeWriterMessage(message, Message.getBattleStartMessage())
+    else
+      Component.addTypeWriterMessage(
+        message,
+        Message.getNewEnemyBattleStartMessage()
+      )
+  }
+
   function displayPlayerMessage(boardElement, ship = false) {
     const agent = document.getElementById('message-agent')
     const enemy = document.getElementById('message-enemy')
@@ -311,53 +390,18 @@ const Battle = (() => {
     }
   }
 
-  function playerHits(data) {
-    const board = document.getElementById('field-container-enemy')
-    const map = data.cpu.getMap()
-    const boardArray = map.getBoard()
-
-    map.receiveAttack([data.row, data.col])
-    if (data.battleship.isSunk) {
-      const element = boardArray[data.row][data.col]
-      const [row, col] = findOrigin(boardArray, boardArray[data.row][data.col])
-      fleet.loadShipOnBoard(data.cpu, { map, board, element, row, col })
-    }
-  }
-
-  function findOrigin(board, element) {
-    for (let i = 0; i < board.length; i += 1) {
-      for (let j = 0; j < board[0].length; j += 1) {
-        if (board[i][j] === element) return [i, j]
-      }
-    }
-    return [0, 0]
-  }
-
-  // INITS / UnINITS
-
-  function initBoardFields() {
-    const enemyMap = document.getElementById('board-enemy')
-    const enemyBoard = enemyMap.querySelector('.field-container')
-    enemyBoard.childNodes.forEach((field) => {
-      field.addEventListener('click', handleFieldClick)
-    })
-    addFieldHoverWhenOnTurn()
-  }
-
-  function unInitBoardFields() {
-    const fields = document.querySelectorAll('.field')
-    fields.forEach((field) =>
-      field.removeEventListener('click', handleFieldClick)
-    )
-    removeFieldHoverWhenOffTurn()
-  }
-
-  // MESSAGES
-
   function displayPlayerNoCommentMessage() {
     const agent = document.getElementById('message-agent')
 
     displayMessage(agent, Message.getNoCommentMessage()[0])
+  }
+
+  function displayWinMessage(character) {
+    const message = document.getElementById(`message-${character}`)
+    if (character === 'agent-win')
+      Component.addTypeWriterMessage(message, Message.getPlayerWinMessage())
+    if (character === 'enemy-win')
+      Component.addTypeWriterMessage(message, Message.getEnemyWinMessage())
   }
 
   function displayMessage(node, message) {
@@ -374,9 +418,7 @@ const Battle = (() => {
     }
   }
 
-  function getShipNameFromBoard(boardElement) {
-    return boardElement.slice(0, boardElement.length - 1)
-  }
+  // STYLIZERS
 
   function removeFieldHoverWhenOffTurn() {
     const container = document.getElementById('field-container-enemy')
